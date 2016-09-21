b4524261635840a26420c0db5d2e63840e71908f
urzp <urzp002@mail.ru>
2016-09-21 23:13:09 +0300

    add all flight  as default

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,15 @@
 class FlightsController < ApplicationController
   def index
-    #puts "params = #{params}"
+    puts "params = #{params}"
+    if params[:date]
     @date = DateTime.parse("#{params[:date][:year]}-#{params[:date][:month]}-#{params[:date][:day]}" )
     #puts "date = #{date.wday.class}"
     #week_day = Date::DAYNAMES[date.wday]
     #@flights = Flight.first(10)
     @flights = Flight.where("id_takeoff = ? AND id_landing = ? AND week_day = ? " , params[:from], params[:to], @date.wday).order(:depart)
+    else
+      @flights = Flight.all
+    end
     @airports = Airport.all.map{|a| [ a.name, a.id ] }
     @passengers = (1..4).map{|a| [ a, a ] }
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921153125) do
+ActiveRecord::Schema.define(version: 20160921191538) do
 
   create_table "airports", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
