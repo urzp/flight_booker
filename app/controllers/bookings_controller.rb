af723f32080f4c9fcbb8a0916ce91e68556b72c0
urzp <urzp002@mail.ru>
2016-09-22 22:48:08 +0300

    add passengeres for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,10 @@ class BookingsController < ApplicationController
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
     @booking.flight = @flight
+    @booking.date = params[:date]
+     params[:tikets].to_i.times do
+      #@booking.passenger = Passanger.new
+    end
     puts " params  #{params} "
   end
 
