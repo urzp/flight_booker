4136a51bb1e35c38d45c1ba40e2a9335327b5efc
urzp <urzp002@mail.ru>
2016-09-22 22:03:59 +0300

    pagination

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-
+gem	'bootstrap-will_paginate',	'0.0.10'
+gem	'will_paginate',	'3.0.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7.1'
 
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,7 @@ class FlightsController < ApplicationController
     #@flights = Flight.first(10)
     @flights = Flight.where("id_takeoff = ? AND id_landing = ? AND week_day = ? " , params[:from], params[:to], @date.wday).order(:depart)
     else
-      @flights = Flight.all
+      @flights = Flight.paginate(page:	params[:page])
     end
     @airports = Airport.all.map{|a| [ a.name, a.id ] }
     @passengers = (1..4).map{|a| [ a, a ] }
