7b69163a9a9c52cdaaab26eb7440187047faf299
urzp <urzp002@mail.ru>
2016-09-23 17:42:51 +0300

    head

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,12 +4,22 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @booking.flight = @flight
     @booking.date = params[:date]
-     @count_passangers = params[:tikets].to_i
+    params[:tikets].to_i.times { @booking.passangers.build }
+     #@count_passangers = params[:tikets].to_i
       #@booking.passengers.build
 
-    puts " params  #{params} "
+
   end
 
+  def create
+    puts " params ---->  #{params} "
+     @booking = Booking.new(booking_params)
 
+  end
 
+  private
+   def booking_params
+     params.require(:booking).permit(:flight_id,
+                 :passengers_attributes => [:name, :email] )
+   end
 end
